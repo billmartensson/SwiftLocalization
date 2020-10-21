bfe2067fc6724505653b1b089b25591079466dbd
Bill Mårtensson <bill@magictechnology.se>
2020-10-21 18:54:32 +0200

    Start

--- LanguageTest/ViewController.swift
+++ LanguageTest/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
+    @IBOutlet weak var theTextfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(Locale.preferredLanguages)
+        for lang in Locale.preferredLanguages
+        {
+            let firstTwo = lang.index(lang.startIndex, offsetBy: 2)
+            
+            let langCode = String(lang[lang.startIndex ..< firstTwo])
+            print(langCode)
+        }
+        
+        
+        
+        
     }
 
-
+    
+    @IBAction func letsClick(_ sender: Any) {
+        theLabel.text = NSLocalizedString("Result", comment: "The result text") + ": " + theTextfield.text!
+    }
+    
 }
 
